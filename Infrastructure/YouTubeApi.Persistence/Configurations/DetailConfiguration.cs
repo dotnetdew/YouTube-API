84f36b00d280bd059d97cb03bfea2b64714aef84
Abdullokh Tolibjonov <abdullohprogram@gmail.com>
2024-06-17 10:34:12 +0500

    entity configurations are added

---
+++ Infrastructure/YouTubeApi.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouTubeApi.Domain.Entities;
+
+namespace YouTubeApi.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Faker faker = new ("tr");
+
+            Detail detail1 = new()
+            {
+                Id = 1,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 1,
+                CraetedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            Detail detail2 = new()
+            {
+                Id = 2,
+                Title = faker.Lorem.Sentence(2),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 3,
+                CraetedDate = DateTime.Now,
+                IsDeleted = true
+            };
+
+            Detail detail3 = new()
+            {
+                Id = 3,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 4,
+                CraetedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            builder.HasData(detail1, detail2, detail3);
+        }
+    }
+}
