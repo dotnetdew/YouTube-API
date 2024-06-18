4e78c99a898143018b1dba3d61e48c28f0d82c51
Abdullokh Tolibjonov <abdullohprogram@gmail.com>
2024-06-18 09:25:32 +0500

    GetAsync has been changed in read repo

--- Infrastructure/YouTubeApi.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/YouTubeApi.Persistence/Repositories/ReadRepository.cs
@@ -57,7 +57,7 @@ public class ReadRepository<T> : IReadRepository<T> where T : class, IEntityBase
         
         //queryable.Where(predicate);
 
-        return await queryable.FirstOrDefaultAsync();
+        return await queryable.FirstOrDefaultAsync(predicate);
     }
 
     public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
